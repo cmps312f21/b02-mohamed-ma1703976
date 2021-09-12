dadbc84880896e3cedfdac22f621af9c4c24bdb4
Mohamed Abdelmoety <88920277+mohamed-ma1703976@users.noreply.github.com>
2021-09-13 01:35:57 +0300

    assigment1

--- Assigments/Assigment1/BooksDonation2/app/src/androidTest/java/Book/BooksTest.kt
+++
@@ -1,9 +0,0 @@
-package Book
-
-fun main() {
-    println(BooksRepo.getBook("Android in Action, Second Edition"))
-    BooksRepo.getBooksByPageCount(400).forEach { println(it) }
-    BooksRepo.getBooksByAuthor("W. Frank Ableson").forEach { println(it) }
-    BooksRepo.getBooksByCategory("Internet").forEach { println(it) }
-    BooksRepo.getAuthorsBookCount().forEach { println(it) }
-}
