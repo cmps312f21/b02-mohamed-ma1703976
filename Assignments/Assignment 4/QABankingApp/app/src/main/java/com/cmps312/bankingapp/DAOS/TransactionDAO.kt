5b6c4eebe131acc0c1a3ceb3b3b8daaf85e58044
mohossam3 <88920277+mohamed-ma1703976@users.noreply.github.com>
2021-11-11 04:01:49 +0300

    Assigment

--- Assignments/Assignment 4/QABankingApp/app/src/main/java/com/cmps312/bankingapp/DAOS/TransactionDAO.kt
+++ Assignments/Assignment 4/QABankingApp/app/src/main/java/com/cmps312/bankingapp/DAOS/TransactionDAO.kt
@@ -1,16 +1,10 @@
 package com.cmps312.bankingapp.DAOS
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.Update
 import com.cmps312.bankingapp.models.Transaction
 @Dao
 interface TransactionDAO {
     @Insert
-    suspend fun insert(transaction: Transaction): Long
-    @Update
-    suspend fun update(transaction: Transaction)
-    @Delete
-    suspend fun delete(transaction: Transaction)
+    suspend fun insert(transaction: Transaction)
 }
